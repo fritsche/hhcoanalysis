d1adce766a8e86aba8f1cc7cddab71f6a86f6545
Gian Fritsche <gian.fritsche@gmail.com>
2019-08-28 09:31:09 -0300

    add getSelected() to hhcorandom

--- src/main/java/br/ufpr/inf/cbio/hhcoanalysis/hyperheuristic/HHCORandom.java
+++ src/main/java/br/ufpr/inf/cbio/hhcoanalysis/hyperheuristic/HHCORandom.java
@@ -35,6 +35,11 @@ public class HHCORandom<S extends Solution<?>> extends HHCO<S> {
 
     private CooperativeAlgorithm<S> selected;
 
+    @Override
+    public CooperativeAlgorithm<S> getSelected() {
+        return selected;
+    }
+
     public HHCORandom(List<CooperativeAlgorithm<S>> algorithms, int populationSize, int maxEvaluations, Problem problem) {
         super(algorithms, populationSize, maxEvaluations, problem, "HHCORandom", new RandomSelection<CooperativeAlgorithm>(), new VoidFIR());
     }

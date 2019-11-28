e06d3f0793fd2437bc3a57a0304dcbc408b1368b
Gian Fritsche <gian.fritsche@gmail.com>
2019-11-28 12:41:19 -0200

    add script to run on a subset of MaF problems

---
+++ scripts/runpartial.sh
@@ -0,0 +1,88 @@
+#!/usr/bin/env bash
+
+set -e
+
+source scripts/seeds.sh
+
+if [ "$#" -ne 2 ]; then
+	echo "Expected algorithm <name> and <group>"
+	exit 1
+fi
+
+method="local"
+dir=$(pwd)
+if [ "$method" = "remote" ]; then
+	execute="sbatch -J $name.run $dir/scripts/addjob.sh" # for running on process queue
+	dir="/home/gian/hhcoanalysis/"
+else
+	execute="bash $dir/scripts/addbatch.sh" # for running locally
+fi
+
+mvn package -DskipTests
+
+methodology=MaFMethodology
+algorithm=$1
+runs=20
+replace=false # execute and replace if result exists
+jar=target/HHCOAnalysis-1.0-SNAPSHOT-jar-with-dependencies.jar
+main=br.ufpr.inf.cbio.hhcoanalysis.runner.Main
+javacommand="java -Duser.language=en -cp $jar -Xmx1g $main"
+group=$2
+
+m=5
+problems=(MaF01 MaF02 MaF03 MaF04 MaF05 MaF06 MaF07 MaF08 MaF09 MaF10 MaF11 MaF12 MaF13 MaF14 MaF15)
+seed_index=0
+for problem in "${problems[@]}"; do
+	for (( id = 0; id < $runs; id++ )); do
+		# each objective, problem and independent run (id) uses a different seed
+		seed=${seeds[$seed_index]}
+		# different algorithms on the same problem instance uses the same seed
+		output="$dir/experiment/$methodology/$m/$group/$algorithm/$problem/"
+		file="$output/FUN$id.tsv"
+		if [ ! -s $file ] || [ "$replace" = true ]; then
+			params="-P $output -M $methodology -A OFF -a $algorithm -p $problem -m $m -id $id -s $seed"
+			$execute "$javacommand $params 2>> $algorithm.$seed.log"
+		fi
+		seed_index=$((seed_index+1))
+	done
+done
+
+m=10
+problems=(MaF01 MaF04 MaF05 MaF06 MaF07 MaF09 MaF10 MaF13 MaF15)
+seed_index=0
+for problem in "${problems[@]}"; do
+	for (( id = 0; id < $runs; id++ )); do
+		# each objective, problem and independent run (id) uses a different seed
+		seed=${seeds[$seed_index]}
+		# different algorithms on the same problem instance uses the same seed
+		output="$dir/experiment/$methodology/$m/$group/$algorithm/$problem/"
+		file="$output/FUN$id.tsv"
+		if [ ! -s $file ] || [ "$replace" = true ]; then
+			params="-P $output -M $methodology -A OFF -a $algorithm -p $problem -m $m -id $id -s $seed"
+			$execute "$javacommand $params 2>> $algorithm.$seed.log"
+		fi
+		seed_index=$((seed_index+1))
+	done
+done
+
+m=15
+problems=(MaF01 MaF04 MaF06 MaF07 MaF15)
+seed_index=0
+for problem in "${problems[@]}"; do
+	for (( id = 0; id < $runs; id++ )); do
+		# each objective, problem and independent run (id) uses a different seed
+		seed=${seeds[$seed_index]}
+		# different algorithms on the same problem instance uses the same seed
+		output="$dir/experiment/$methodology/$m/$group/$algorithm/$problem/"
+		file="$output/FUN$id.tsv"
+		if [ ! -s $file ] || [ "$replace" = true ]; then
+			params="-P $output -M $methodology -A OFF -a $algorithm -p $problem -m $m -id $id -s $seed"
+			$execute "$javacommand $params 2>> $algorithm.$seed.log"
+		fi
+		seed_index=$((seed_index+1))
+	done
+done
+
+rm job.log
+
+
